96525465c3bd7c84b13eca89bf181079439a2bbe
LuanPiauilino <luanpsilva2001@gmail.com>
2022-07-08 21:19:24 -0300

    Criando a classe Jogo e atribuindo CRUD a ela

---
+++ src/main/java/com/generation/lojagames/model/Jogo.java
@@ -0,0 +1,74 @@
+package com.generation.lojagames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name= "tb_jogo")
+public class Jogo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank(message = "Necessário inserir o nome do jogo.")
+	private String nome;
+	
+	@NotBlank(message = "Necessário inserir quais plataforma é adequado para esse jogo")
+	private String plataforma;
+	
+	@NotNull
+	private double preco;
+	
+	@NotBlank(message = "Neessário inserir o nome da empresa que criou o jogo.")
+	private String empresaCriador;
+
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getEmpresaCriador() {
+		return empresaCriador;
+	}
+
+	public void setEmpresaCriador(String empresaCriador) {
+		this.empresaCriador = empresaCriador;
+	}
+	
+	
+}
